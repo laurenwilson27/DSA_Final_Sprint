750ba6f72e90b284cb4ec0e443f5a263df936c5e
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2024-08-02 03:04:51 -0230

    Initialisation for Maven w/ Spring, Postgres, Thymeleaf

---
+++ src/main/java/com/keyin/lrw/sprint2/DsaFinalSprintApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.lrw.sprint2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaFinalSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaFinalSprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyin/lrw/sprint2/DsaFinalSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.lrw.sprint2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaFinalSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
